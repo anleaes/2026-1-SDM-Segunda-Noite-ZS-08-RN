348501a0bccdf76e50443fac1b6bcb6f9e605c86
Nathan Ferreira <nathansferreira469@gmail.com>
2026-05-31 17:28:31 -0300

    cria HomeScren e adiciona importações

---
+++ academiaappreactnative/screens/HomeScreen.tsx
@@ -0,0 +1,4 @@
+import { DrawerScreenProps } from '@react-navigation/drawer';
+import React from 'react';
+import { Text, View } from 'react-native';
+import { DrawerParamList } from '../navigation/DrawerNavigator';
