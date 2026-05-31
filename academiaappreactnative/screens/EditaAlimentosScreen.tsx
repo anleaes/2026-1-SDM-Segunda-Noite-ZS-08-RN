4cc31fd1f3cc1cc6020562f4ef3a2e70e933df59
Nathan Ferreira <nathansferreira469@gmail.com>
2026-05-31 17:46:05 -0300

    cria screen EditaAlimentos e adiciona importações

---
+++ academiaappreactnative/screens/EditaAlimentosScreen.tsx
@@ -0,0 +1,4 @@
+import { DrawerScreenProps } from '@react-navigation/drawer';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { DrawerParamList } from '../navigation/DrawerNavigator';
