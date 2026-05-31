d0c5296417de837f6e5d7224d364c9153090ed55
Nathan Ferreira <nathansferreira469@gmail.com>
2026-05-31 17:40:58 -0300

    cria screen CriaAlimentos e adiciona importações

---
+++ academiaappreactnative/screens/CriaAlimentosScreen.tsx
@@ -0,0 +1,5 @@
+import { DrawerScreenProps } from '@react-navigation/drawer';
+import { useFocusEffect } from 'expo-router';
+import React, { useCallback, useState } from 'react';
+import { ActivityIndicator, Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { DrawerParamList } from '../navigation/DrawerNavigator';
